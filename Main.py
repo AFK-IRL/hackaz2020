c0fa238189bd195882fb61ee8c6274791eeab582
tptrw61 <tptrw.61@gmail.com>
2020-01-18 00:08:46 -0700

    dealt with window size problem

--- Main.py
+++ Main.py
@@ -17,6 +17,19 @@ win.keypad(True)
 # Init window
 char_height = 40
 char_width = 160
+
+actual_height, actual_width = win.getmaxyx()
+
+if char_height != actual_height or char_width != actual_width:
+    curses.echo()
+    curses.nocbreak()
+    win.keypad(False)
+    curses.endwin()
+    print(f"Window size must be {char_width}x{char_height}")
+    print(f"Window size is currently {actual_width}x{actual_height}")
+    print("Please resize window and relaunch")
+    exit(1)
+
 begin_y = 0
 begin_x = 0
 #curses.resizeterm(char_height, char_width)
