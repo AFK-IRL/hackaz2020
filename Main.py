d0daa32595835d09a78abcedc46baa7593ad044c
tptrw61 <tptrw.61@gmail.com>
2020-01-18 11:21:27 -0700

    added combat context switch

--- Main.py
+++ Main.py
@@ -20,17 +20,20 @@ curses.cbreak()
 win.keypad(True)
 
 # Init window
+def resetConsole():
+    curses.echo()
+    curses.nocbreak()
+    win.keypad(False)
+    curses.curs_set(default_cursor_visibility)
+    curses.endwin()
+
 char_height = 35
 char_width = 90
 
 actual_height, actual_width = win.getmaxyx()
 
 if char_height > actual_height or char_width > actual_width:
-    curses.echo()
-    curses.nocbreak()
-    win.keypad(False)
-    curses.curs_set(default_cursor_visibility)
-    curses.endwin()
+    resetConsole()
     print(f"Window size must be at least {char_width}x{char_height}")
     print(f"Window size is currently {actual_width}x{actual_height}")
     print("Please resize window and relaunch")
@@ -41,15 +44,22 @@ begin_x = 0
 #curses.resizeterm(char_height, char_width)
 win.refresh()
 
+inCombat = False
+currentEnemy = None
 gameOver = False
 
-while not gameOver:
-    #win.addstr(map._map)
+
+
+def updateInMap():
+    global gameOver, inCombat, currentEnemy
     row = 0
     for line in control.levelMap.getStrings():
         win.addstr(row, 0, line)
         #print(control.levelMap.getStrings()[0][control.player.y][control.player.x])
         row += 1
+
+    #win.addstr(30, 0, f"({control.player.x}, {control.player.y})             ")
+    #win.addstr(31, 0, str([(enemy.x,enemy.y) for enemy in control.enemies]))
 
     ch = win.getkey()
 
@@ -63,12 +73,46 @@ while not gameOver:
         control.player.move_right()
     if ch == "q":
         gameOver = True
+        return
 
+    # check if player is on enemy
+    for i in range(len(control.enemies)):
+        if control.player.x == control.enemies[i].x and control.player.y == control.enemies[i].y:
+            inCombat = True
+            currentEnemy = i
+            #win.addstr(32, 0, "combat!!")
+            return updateInCombat()
+
+    # TODO move enemies
+
+    # check if enemy moved onto player
+    for i in range(len(control.enemies)):
+        if control.player.x == control.enemies[i].x and control.player.y == control.enemies[i].y:
+            inCombat = True
+            currentEnemy = i
+            return updateInCombat()
+
+def updateInCombat():
+    global gameOver, inCombat, currentEnemy
+    for i in range(30):
+        win.addstr(i, 0, ' '*90)
+    win.addstr(1, 1, "combat goes here...")
+    win.addstr(2, 1, "press any key to continue (q still quits)")
+    win.addstr(32, 0, "        ")
+    ch = win.getkey()
+    if ch == "q":
+        gameOver = True
+        return
+    inCombat = False
+    currentEnemy = None
+
+while not gameOver:
+    #win.addstr(33, 0, f'{gameOver}')
+    if inCombat:
+        updateInCombat()
+    else:
+        updateInMap()
     win.refresh()
 
 # Terminates curses application
-curses.echo()
-curses.nocbreak()
-win.keypad(False)
-curses.curs_set(default_cursor_visibility)
-curses.endwin()
+resetConsole()
