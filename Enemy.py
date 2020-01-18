7758099a5ee42a813a026da9355e1fae2e7d710f
nicrobkal <nicrobkal@gmail.com>
2020-01-17 21:38:14 -0700

    Added enemy random movement

--- Enemy.py
+++ Enemy.py
@@ -1,3 +1,5 @@
+import random
+
 class Enemy:
 
     def __init__(self, x, y):
@@ -5,6 +7,23 @@ class Enemy:
         self.y = y
         self.x_bound = 0
         self.y_bound = 0
+        self.prob_to_move = .5
+
+    def move_random(self):
+        if random.random() < prob_to_move:
+            num = random.randint(1,4)
+            if num == 1:
+                self.move_left()
+            elif num == 2:
+                self.move_right()
+            elif num == 3:
+                self.move_up()
+            else:
+                self.move_down()
+
+        }
+        
+        return switcher.get(random.randInt(0,4))
 
     def move_left(self):
         self.x -= 1
