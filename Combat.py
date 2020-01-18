76a033f87977c2255ac691891bddb8597389318d
Cameron Bryce Larson <cblarson@email.arizona.edu>
2020-01-18 15:03:07 -0700

    Combat v0.1 done.

---
+++ Combat.py
@@ -0,0 +1,53 @@
+import random
+
+class Combat:
+
+    def __init__(self, win, player, enemy):
+        self._win = win
+        self._player = player
+        self._enemy = enemy
+        self._word_dic = { 0: ['hello', 'could', 'weight'], 1: ['frustrating', 'complicated', 'ruminated'], 2: ['equanimity', 'blandishment', 'circumlocution']}
+        self._cur_word = ''
+        self._cur_word_list = []
+
+    
+    def setup(self):
+        choice = random.randint(0,3)
+
+        self._cur_word = self._word_dic[self._enemy.difficulty][choice]
+        self._cur_word_list = self._cur_word.split('')
+
+    # returns 0 if enemy died, returns 1 if player died
+    def fight(self):
+        
+        while True:
+            self.setup()
+            spelling = True
+            while len(self._cur_word_list) > 0 and spelling:
+                ch = self._win.getkey()
+
+                if ch != self._cur_word_list[0]:
+                    spelling = False
+                
+                self._cur_word_list.pop(0)
+
+            damage = 0
+            if spelling:
+                damage = self._player.use_weapon()
+
+            self._enemy.take_damage(damage)
+
+            if self._enemy.is_dead():
+                return 0
+
+            self._player.take_damage(self._enemy.attack())
+            if self._player.is_dead():
+                return 1
+
+    def get_cur_word(self):
+        return self._cur_word
+        
+
+    
+
+    
--- Enemy.py
+++ Enemy.py
@@ -2,13 +2,18 @@ import random
 
 class Enemy:
 
-    def __init__(self, x, y):
+    # difficulty is integer 0-2 (inclusive)
+    def __init__(self, x, y, difficulty, health, damage, miss_chance):
         self.x = x
         self.y = y
         self.x_bound = 0
         self.y_bound = 0
         self.prob_to_move = .5
         self.alive = True
+        self.difficulty = difficulty
+        self.health = health
+        self.damage = damage
+        self.miss_chance = miss_chance
 
     def move_random(self):
         if random.random() < self.prob_to_move:
@@ -33,3 +38,15 @@ class Enemy:
     
     def move_down(self):
         self.y += 1
+
+    def take_damage(self, amt):
+        self.health -= amt
+
+    def is_dead(self):
+        return self.health < 1
+
+    def attack(self):
+        chance = random.randint(1, 100)
+        if chance > self.miss_chance:
+            return self.damage
+        return 0
