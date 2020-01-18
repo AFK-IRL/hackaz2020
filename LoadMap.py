6cd9d220c88a0bcc90b2244f26ce3e978ad4cbca
tptrw61 <tptrw.61@gmail.com>
2020-01-17 23:20:16 -0700

    removed pycache, Map constructor finished

--- LoadMap.py
+++ LoadMap.py
@@ -3,12 +3,24 @@ class Map:
 
     def __init__(self, fileName):
         self._fileName = fileName
-        self._map = [[]]
+        self._map = []
         self.player = None
         self.enemies = None
 
-        with file(fileName, 'r') as f:
-            pass
+        lines = []
+        with open(fileName, 'r') as f:
+            lines = f.readLines()
+        for line in lines:
+            line = line.replace('\n', '')
+            line = line.replace('\r', '')
+            arr = line.split()
+            for i in range(len(arr)):
+                if arr[i] == ' ':
+                    arr[i] = 0
+                else:
+                    arr[i] = 1
+            self._map += [arr]
+
 
     def update(self):
         return None
